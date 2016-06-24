d05f0e075457f0c05b42e98ff8e564400faa977a
Mitch Monsen <mmonsen7@gmail.com>
2016-06-24 13:50:29 -0600

    adds test for number validator case with blank string

--- test/rulix/validators/test_number_validator.rb
+++ test/rulix/validators/test_number_validator.rb
@@ -17,6 +17,14 @@ class TestNumberValidator < MiniTest::Test
     assert_equal true, result
   end
 
+  def test_with_blank_string
+    validator = Rulix::Validators::NumberValidator.new
+    value = ''
+    result = validator.call value
+
+    assert_equal([false, "is not a number"], result)
+  end
+
   def test_error_call
     validator = Rulix::Validators::NumberValidator.new
     value = '123 Main Street'
